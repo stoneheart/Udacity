408817ac17d7127504e1325da59fa3b72a7c241f
stoneheart <kz9892@gmail.com>
2016-03-11 16:40:07 -0800

    Update Project3

---
+++ Project3/fetch_html.py
@@ -0,0 +1,21 @@
+from bs4 import BeautifulSoup
+import requests
+s = requests.Session()
+
+r = s.get("http://www.transtats.bts.gov/Data_Elements.aspx?Data=2")
+soup = BeautifulSoup(r.text, "lxml")
+viewstate_element = soup.find(id="__VIEWSTATE")
+viewstate = viewstate_element["value"]
+eventvalidation_element = soup.find(id="__EVENTVALIDATION")
+eventvalidation = eventvalidation_element["value"]
+
+r = s.post("http://www.transtats.bts.gov/Data_Elements.aspx?Data=2",
+          data={'AirportList':"BOS",
+                'CarrierList':"VX",
+                'Submit':"Submit",
+                '__EVENTTARGET':"",
+                '__EVENTARGUMENT':"",
+                '__EVENTVALIDATION':eventvalidation,
+                '__VIEWSTATE':viewstate})
+f = open("virgin_and_logan_airport.html", "w")
+f.write(r.text)
